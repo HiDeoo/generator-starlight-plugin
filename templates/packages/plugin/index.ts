3533b64bed0dd84b8f785eff610cab267bdfa7e0
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-09-01 15:00:36 +0200

    feat: plugin entry point example

--- templates/packages/plugin/index.ts
+++ templates/packages/plugin/index.ts
@@ -4,8 +4,17 @@ export default function <%= importName %>(): StarlightPlugin {
   return {
     name: '<%= name %>',
     hooks: {
-      setup() {
-        // TODO(HiDeoo)
+      setup({ logger }) {
+        /**
+         * This is the entry point of your Starlight plugin.
+         * The `setup` hook is called when Starlight is initialized (during the Astro `astro:config:setup` integration
+         * hook).
+         * To learn more about the Starlight plugin API and all available options in this hook, check the Starlight
+         * plugins reference.
+         *
+         * @see https://starlight.astro.build/reference/plugins/
+         */
+        logger.info('Hello from the <%= name %> plugin!')
       },
     },
   }

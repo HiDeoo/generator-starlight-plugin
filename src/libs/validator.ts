9a1086203e9ebeb90d0f012647bdabccdd479254
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-09-02 09:45:06 +0200

    chore: fix error message typo

--- src/libs/validator.ts
+++ src/libs/validator.ts
@@ -1,5 +1,5 @@
 const nameValidationRegex = /^(?:@[\da-z~-][\d._a-z~-]*\/)?[\da-z~-][\d._a-z~-]*$/
 
 export function validateName(name: string) {
-  return nameValidationRegex.test(name) ? true : 'Invalid plugin na me'
+  return nameValidationRegex.test(name) ? true : 'Invalid plugin name'
 }

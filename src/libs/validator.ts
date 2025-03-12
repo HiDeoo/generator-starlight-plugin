1163179d66cd183147fea787d316435194d2f67e
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-03-12 17:06:02 +0100

    feat: add emoji prompt

--- src/libs/validator.ts
+++ src/libs/validator.ts
@@ -1,4 +1,5 @@
 const nameValidationRegex = /^(?:@[\da-z~-][\d._a-z~-]*\/)?[\da-z~-][\d._a-z~-]*$/
+const emojiValidationRegex = /^\p{Emoji_Presentation}\p{Emoji_Modifier}*$/u
 
 export function validateName(name: string) {
   return nameValidationRegex.test(name) ? true : 'Invalid plugin name'
@@ -6,4 +7,8 @@ export function validateName(name: string) {
 
 export function validateNonEmptyString(value: string) {
   return value.trim().length > 0 ? true : 'Value cannot be empty'
+}
+
+export function validateEmoji(emoji: string) {
+  return emojiValidationRegex.test(emoji) ? true : 'Invalid emoji'
 }
